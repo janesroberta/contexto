265d44a7e2d0190728fcb3869afafd2448b25c0e
Janes Roberta <108099951+janesroberta@users.noreply.github.com>
2022-12-13 10:17:41 -0300

    finalizado

---
+++ script.js
@@ -0,0 +1,240 @@
+var wordsList = ['arbitrário','auspicioso','anacrônico','alteridade','abominável','apreciação','antagônico','atribulado','apresentar','atribuição','abundância','apascentar','autoridade','antológico','assembleia','apreensivo','autoestima','arrogância','assessoria','abrangente','ascendente','beneplácito','benevolência','beligerante','benignidade','brio','bizarro','benevolente','bem','basculho','buscar','boçal','brado','bíblia','balbúrdia','bom','bastardo','bélico','bordão','benefício','brando','banal','condescendente','cônjuge','cínico','caráter','concessão','corroborar','condolências','contemporâneo','concepção','complacência','corolário','contundente','característica','conceito','convém','condescendência','comorbidade','compreensão','circunstância','conserto','cultura','dicionário','denegrir','dissimulado','diligência','deferido','detrimento','dicotomia','desgraça','discrepância','disruptivo','devaneio','determinação','deliberar','discernimento','demasiado','descrição','diligente','despeito','destarte','disposição','empatia','embuste','exceção','exceto','efêmero','escrúpulo','equidade','essência','extrovertido','estória','essencial','eminente','extensão','empírico','esdrúxulo','encarecidamente','extraordinário','expectativa','esperança','escopo','estigma','fato','família','fomentar','fazer','fidedigno','felicitações','fleumático','fosse','frívolo','fulguras','formal','finalidade','fomento','fútil','facção','felicidade','fático','facultativo','fundamento','formidável','fraternidade','genocida','genuíno','gratidão','gentil','genérico','glória','generoso','genro','graça','generalizar','gleba','grande','garrida','grotesco','ganância','gênese','gênero','guisa','garboso','genioso','hipócrita','hegemonia','hermético','heterogeneidade','hipocrisia','hostil','história','hábito','honra','híbrido','harmonia','humildade','hesitar','heterogêneo','haver','holístico','hábil','homogêneo','hipótese','hipotético','imprescindível','inerente','intrínseco','impressão','intempérie','idôneo','idílico','iminente','implícito','invasivo','inconveniente','inspiração','idiossincrasia','inexorável','incidente','inócuo','incipiente','inferir','intrinsecamente','indulgente','independente','iniquidade','jurídico','jactância','julgar','justo','júbilo','justiça','jeito','juízo','jugo','jamais','justificar','joia','jovem','justaposição','jovial','jurista','junto','jocoso','jornada','jazia','kafkaesco','kit','kafkiano','kamikaze','ketchup','karaokê','kiwi','kaiser','kilo','know-how','kardecista','kardecismo','kitchenette','kung fu','kitsch','kelvin','isonjeado','legado','litígio','limiar','lábaro','limítrofe','ludibriar','liberdade','límpido','leniência','lapso','leviano','lealdade','libertinagem','linear','lírico','latente','lunático','lograr','logradouro','mito','mister','mexer','modéstia','mérito','mesquinho','monótono','medíocre','mórbido','mútua','metódico','mitigar','maestria','moral','mal','mediante','muito','modesto','molestar','néscio','negro','nobre','não','nocivo','notívago','necessidade','nostalgia','não obstante','no entanto','notório','necessário','nuance','negligência','natureza','nômade','neófito','nevrálgico','neném','nativo','ordinário','obstante','objetivo','orgulho','oriundo','otário','obstinado','outrossim','ocioso','outrem','opróbrio','ousadia','objeto','oportunista','oblíquo','olvidar','orgânico','opinião','obséquio','outorgar','obsoleto','perseverança','perspicaz','peculiar','perspectiva','prescindir','presunçoso','prolixo','prudente','paradigma','presunção','pandemia','prerrogativa','propósito','parcimônia','plenitude','pêsames','peremptório','premissa','procrastinar','prepotente','quimera','quiçá','qualidade','quando','quesito','que','quinhão','quanto','qualquer','questão','qualitativo','quintessência','querer','querela','quarentena','quer','quão','quaisquer','quiser','quilombo','respeito','resiliência','recíproco','reciprocidade','remanescente','retificar','ratificar','referência','reiterar','relativo','redimir','recesso','resignação','refutar','remissão','respaldo','resignado','regozijar','retórica','rechaçar','retrógrado','significado','sagaz','supérfluo','sublime','sucinto','sororidade','serenidade','senso','sagacidade','sapiência','subterfúgio','solidariedade','soberba','sinônimo','subestimar','suscetível','síntese','simultaneamente','sutil','superficial','sucumbir','talarico','termo','trivial','transcender','tênue','tribulação','transgressão','taciturno','também','torpe','transeunte','transcendente','ter','tange','tergiversar','trabalho','tendência','tempo','tangível','tolo','todavia','unidade','unguento','unilateral','urbano','uníssono','ultraje','usurpar','umbral','usar','ubíquo','utensílio','unívoco','ufanar','unicidade','universal','utópico','ubiquidade','uniformidade','ultrajado','urdidura','unanimidade','unificado','unidade federativa','uniforme','urgente','verbete','vicissitude','viés','vereda','vagabundo','você','virtude','vislumbrar','vigor','vocábulo','verossimilhança','veemente','veemência','vitupério','vulgar','varonil','volúvel','versátil','vertente','volátil','vigente','vigoroso','verossímil','victor','weberiano','windsurfe','wattímetro','wagnerismo','wildiano','warrantar','wurtzita','wulfenita','warrantado','wagnerista','wagnerita','windsurfista','wollastonita','wronskiano','wagneriana','workaholics','web design','weber','western','wagneriano','warrantagem','xenofobia','xingar','xeque','xícara','xará','xale','xerife','xodó','xenofóbico','xaveco','xucro','xadrez','xampu','xepa','xenófobo','xereta','xácara','xexelento','xingamento','zelo','zombar','zelar','zeloso','zangado','zoológico','zumbi','zombeteiro','zoar','zoeira','zen','zoada','zombaria','zênite','zona','zoomórfico','zero','zebra','não','paz','fel','vil','sob','céu','ser','mal','ver','mãe','ter','ego','bem','mas','vão','vir','bom','dar','dom','ora','são','amor','fato','viés','mito','você','caos','como','esmo','brio','vide','ação','após','sede','pois','vida','auge','saga','casa','ônus','ermo','medo','dicionário','prescindir','intrínseco','presunçoso','diligência','corroborar','intempérie','detrimento','parcimônia','referência','inspiração','inexorável','prepotente','pragmático','incipiente','sororidade','disruptivo','serenidade','indulgente','arbitrário','auspicioso','mesquinhez','intrepidez','insensatez','desfaçatez','embriaguez','imperatriz','sobrepeliz','desinfeliz','automotriz','quilohertz','pega-rapaz','quebra-luz','tartamudez','esqualidez','testarudez','malcriadez','ultraveloz','santa-cruz','separatriz','excitatriz','destimidez','inexorável','excêntrico','excelência','exacerbado','excludente','extremista','expediente','extrínseco','embaixador','exuberante','explanação','existência','extenuante','expressivo','exatamente','inflexível','exiguidade','extrapolar','explicação','exasperado','preconizar','sintetizar','mesquinhez','condizente','hostilizar','intrepidez','insensatez','politizado','martirizar','macambúzio','viabilizar','pulverizar','delicadeza','desfaçatez','realização','profetizar','polarizado','reproduzir','civilizado','reutilizar','higienizar']
+
+var gamesList = ["contem","contem","contem","contem","contem","contem","tamanho","tamanho","tamanho","tamanho","inicial","inicial","inicial","inicial","final","final","final","final","final","final","plural","verbo",]
+
+var word = ""
+var game = ""
+var typed = []
+var pontos = 0
+var rodadas = 10
+var gameName = ""
+var historico = ""
+var listaRegra = []
+var tentativasTotal = 0
+var pts = document.getElementById("pts")
+var jogo = document.getElementById("jogo")
+var bot = document.getElementById("bot")
+var last = document.getElementById("last")
+var tipWords = document.getElementById("tipWords")
+var tipWrong = document.getElementById("tipWrong")
+var tentativas = document.getElementById("tentativas")
+var palavraTentativa = document.getElementById("word")
+
+addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("send").click();
+    }
+})
+
+// Enviar
+function Send() {
+    var wordTyped = document.getElementById("word").value
+    var word = wordTyped.toLowerCase()
+    tentativasTotal += 1
+    document.getElementById("word").value = ""
+    if (word == "zerar") {
+        zerar()
+        last.innerHTML = ""
+        typed = []
+    }
+    else if (word.length <= 2) {
+        last.innerHTML = "<p class='small'>Digite uma palavra válida</p>"
+        tentativasTotal = tentativasTotal - 1
+    }
+    else if (/\d+/.test(word) == true) {
+        last.innerHTML = "<p class='small'>Digite uma palavra</p>"
+        tentativasTotal = tentativasTotal - 1
+    }
+    else if (typed.includes(word)) {
+        last.innerHTML = "<p class='small'>Você não pode repetir</p>"
+        tentativasTotal = tentativasTotal - 1
+    }
+    else if (word == "") { tentativasTotal = tentativasTotal - 1 }
+    else if (tentativasTotal == 1) {
+        game = gamesList[Math.floor(Math.random()*gamesList.length)]
+        dicaPalavra = wordsList[Math.floor(Math.random()*wordsList.length)]
+        tentativas.innerHTML = tentativasTotal 
+        bot.innerHTML = 'digite "zerar" para resetar'
+        last.innerHTML = ""
+        ifGame(word, dicaPalavra)
+    }
+    else if (tentativasTotal == rodadas-1) {
+        last.innerHTML = "<p class='small'>Última tentativa</p>"
+        tentativas.innerHTML = tentativasTotal
+        lastTry = pontos
+        ifGame(word, dicaPalavra)
+    }
+    else {
+        last.innerHTML = ""
+        ifGame(word, dicaPalavra)
+    }
+    typed.push(word)
+    tentativas.innerHTML = tentativasTotal 
+}
+
+function zerar() {
+    typed = []
+    pontos = 0
+    historico = ""
+    dicaContem = ""
+    listaRegra = []
+    tentativasTotal = 0
+    jogo.innerHTML = "-"
+    pts.innerHTML = pontos
+    tipWords.innerHTML = historico
+    tentativas.innerHTML = tentativasTotal
+}
+
+function novo() {
+    typed = []
+    historico = ""
+    dicaContem = ""
+    listaRegra = []
+    tentativasTotal = 0
+    jogo.innerHTML = "-"
+    pts.innerHTML = pontos
+    tipWords.innerHTML = historico
+    tentativas.innerHTML = tentativasTotal
+}
+
+function ifGame(word, dicaPalavra) {
+    if (game == "inicial") {
+        gameInicial(word, dicaPalavra)
+    }
+    else if (game == "final") {
+        gameFinal(word, dicaPalavra)
+    }
+    else if (game == "tamanho") {
+        gameTamanho(word, dicaPalavra)
+    }
+    else if (game == "verbo") {
+        gameVerbo(word)
+    }
+    else if (game == "plural") {
+        gamePlural(word)
+    }
+    else if (game == "contem") {
+        gameContem(word, dicaPalavra)
+    }
+}
+
+function gameScript(listaRegra, regraDica, regraWord, word) {
+    if (listaRegra.length > 0) {
+        newTip = listaRegra[0]
+        listaRegra.splice(listaRegra.lastIndexOf(newTip), 1)
+        while (typed.includes(newTip)) {
+            if (listaRegra.length > 0) {
+                newTip = listaRegra[0]
+                listaRegra.splice(listaRegra.lastIndexOf(newTip), 1)
+            }
+            else {
+                newTip = "Acabou as dicas :/"
+            }
+        }
+        typed.push(newTip)
+    }
+    else {
+        newTip = "Acabou as dicas :/"
+    }
+    if (regraDica == regraWord) {
+        rightWord(word, newTip)
+    }
+    else {
+        wrongWord(word, newTip)
+    }
+}
+
+function rightWord(word, newTip) {  
+    historico = '<div class="tip dica"><p>' + newTip + '</p><p class="checkTip">dica</p></div>' + '<div class="tip right"><p>' + word + '</p><p class="checkCorrect">+4pts</p></div>' + historico
+    tipWords.innerHTML = historico
+    pontos += 4
+    pts.innerHTML = pontos
+    if (tentativasTotal == rodadas) {
+        last.innerHTML = "<p class='big grn'>Você adivinhou! +20pts</p><p class='medium'> Resposta: " + gameName + "</p>"
+        pontos += 16 
+        novo()
+        bot.innerHTML = 'digite qualquer palavra iniciar o próximo jogo'  
+    }
+}
+
+function wrongWord(word, newTip) {
+    historico = '<div class="tip dica"><p>' + newTip + '</p><p class="checkTip">dica</p></div>' + '<div class="tip wrong"><p>' + word + '</p><p class="checkWrong">-1pt</p></div>' + historico
+    tipWords.innerHTML = historico
+    if (pontos > 10) {
+        pontos += -1
+        pts.innerHTML = pontos
+    }   
+    if (tentativasTotal == rodadas) {
+        last.innerHTML = "<p class='big red'>Você não adivinhou! -5pts</p><p class='medium'> Resposta: " + gameName + "</p>"
+        if (pontos > 10) {
+            pontos += -1
+            pts.innerHTML = pontos
+        }
+        novo()
+        bot.innerHTML = 'digite qualquer palavra iniciar o próximo jogo'  
+    }
+}
+
+function gameInicial(word, dicaPalavra) {
+    var primeiraLetraWord = word[0]
+    if (tentativasTotal == 1) {
+        listaRegra = wordsList.filter(x => x[0] == dicaPalavra[0])
+        gameName = 'Palavras a primeira letra ' + dicaPalavra[0].toUpperCase()
+        jogo.innerHTML = "Fácil"
+    }
+    gameScript(listaRegra, dicaPalavra[0], primeiraLetraWord, word)
+}
+
+
+function gameFinal(word, dicaPalavra) {
+    var ultimaLetraWord = word.slice(-1)
+    if (tentativasTotal == 1) {
+        listaRegra = wordsList.filter(x => x.slice(-1) == dicaPalavra.slice(-1))
+        gameName = 'Palavras a última letra ' + dicaPalavra.slice(-1).toUpperCase()
+        jogo.innerHTML = "Médio"
+    }
+    gameScript(listaRegra, dicaPalavra.slice(-1), ultimaLetraWord, word)
+}
+
+function gameTamanho(word, dicaPalavra) {
+    var tamanhoWord = word.length
+    if (tentativasTotal == 1) {
+        listaRegra = wordsList.filter(x => x.length == dicaPalavra.length)
+        gameName = 'Palavras com ' + dicaPalavra.length + ' letras'
+        jogo.innerHTML = "Difícil"
+    }
+    gameScript(listaRegra, dicaPalavra.length, tamanhoWord, word)
+}
+
+function gameVerbo(word) {
+    var wordVerbo = word.slice(-2)
+    if (tentativasTotal == 1) {
+        listaRegra = wordsList.filter(x => x.slice(-2) == "ar")
+        gameName = 'Verbos'
+        jogo.innerHTML = "Difícil"
+    }
+    gameScript(listaRegra, "ar", wordVerbo, word)
+}
+
+function gamePlural(word) {
+    var wordPlural = word.slice(-1)
+    if (tentativasTotal == 1) {
+        listaRegra = wordsList.filter(x => x.slice(-1) == "s")
+        gameName = 'Palavras no plural'
+        jogo.innerHTML = "Difícil"
+    }
+    gameScript(listaRegra, "s", wordPlural, word)
+}
+
+var dicaContem = ""
+function gameContem(word, dicaPalavra) {
+    if (tentativasTotal == 1) {
+        dicaContem = dicaPalavra[1]
+        listaRegra = wordsList.filter(x => x.includes(dicaPalavra[1]))
+        gameName = 'Palavras que contém a letra' + dicaPalavra[1].toUpperCase()
+        jogo.innerHTML = "Difícil+"
+    }
+    var wordContem = word.includes(dicaContem)
+    gameScript(listaRegra, true, wordContem, word)
+}
